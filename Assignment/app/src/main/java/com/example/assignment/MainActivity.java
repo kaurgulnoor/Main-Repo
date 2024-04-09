4aa927d7e7cc25150e5c764daebab5ae79e40ef2
kaurgulnoor <159954945+kaurgulnoor@users.noreply.github.com>
2024-04-09 18:22:01 +0300

    Adding compound buttons

--- Assignment/app/src/main/java/com/example/assignment/MainActivity.java
+++ Assignment/app/src/main/java/com/example/assignment/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.assignment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +12,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.assignment.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding variableBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        TextView myText = findViewById(R.id.textView);
+        EditText editText = findViewById(R.id.editTextText);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                String editString = editText.getText().toString();
+                myText.setText("Your edited text has: $editString" + editString);
+            }
         });
-    
     }
 }
